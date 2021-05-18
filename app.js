0915d92e1c755c337b6dbf3a88d8733222144ea4
Matthew Faris <mtfaris90@gmail.com>
2021-05-18 13:39:45 -0400

    add class change on click and css to create functional game

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-console.log("log");
+let playerOnesTurn = true;
 
+document.addEventListener(
+  "click",
+  function (event) {
+    if (!event.target.matches(".tile")) return;
+
+    event.preventDefault();
+    if (playerOnesTurn && !event.target.classList.contains("o")) {
+      event.target.classList.add("x");
+      playerOnesTurn = !playerOnesTurn;
+    } else if (!playerOnesTurn && !event.target.classList.contains("x")) {
+      event.target.classList.add("o");
+      playerOnesTurn = !playerOnesTurn;
+    }
+  },
+  false
+);
